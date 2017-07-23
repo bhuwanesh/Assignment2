523124f373e3ede913a0d1b1465cc029a4cabed7
bhuwanesh <bhuwaneshpareek@gmail.com>
2017-07-23 17:40:21 +0530

    Responsive Web Page
    Responsive Web Page completed in one day using only HTML5 and CSS3

---
+++ dist/menu-minified.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".mobile-button a").click(function(t){$(this).parent().parent().toggleClass("open"),$(this).html("Menu X"==$(this).html()?"Menu":"Menu X"),t.preventDefault()})});
